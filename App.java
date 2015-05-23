f356490c40286f67d67ef4ec792345cee29afd2e
megatricycle <megatricycle@gmail.com>
2015-05-23 17:48:45 +0800

    Added basic BGM music

--- App.java
+++ App.java
@@ -5,20 +5,18 @@
 import java.awt.*;
 import javax.swing.*;
 import java.applet.*;
-import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class App {
 	//ATTRIBUTES
 	// Current game
-	private static Game ongoingGame;
-	private static GameHistory history;
+	private Game ongoingGame;
+	private GameHistory history;
 
-	public static void main(String[] args) throws Exception{
+	public App() throws Exception{
 		//this will serve as the main application
-		URL ingameBG = null;
-		ingameBG = new URL("assets/sounds/bgm/ingame.mp3");
-		System.out.println(ingameBG.toString());
-
 		JFrame frame = new JFrame();
 		//set frame settings
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,6 +25,15 @@ public class App {
 		frame.setUndecorated(true);
 		frame.pack();
 		frame.setVisible(true);
+
+		//start bgm
+		AudioInputStream audioInputStream =
+        AudioSystem.getAudioInputStream(
+            this.getClass().getResource("assets/sounds/bgm/ingame.wav"));
+    Clip clip = AudioSystem.getClip();
+    clip.open(audioInputStream);
+    clip.start();
+		// clip.loop(Clip.LOOP_CONTINUOUSLY); //IF NEEDED LOOP, MOST LIKELY FOR MENU BGM
 
 		Container cp = frame.getContentPane();
 
