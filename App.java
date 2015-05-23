c86c96168bb9c65d666855ae566e47a408c5e606
megatricycle <megatricycle@gmail.com>
2015-05-23 16:03:05 +0800

    Initial sound assets and basic full screen GUI

--- App.java
+++ App.java
@@ -2,6 +2,10 @@
     The main application object. Stores the current game session, game history, and frames.
 */
 
+import java.awt.*;
+import javax.swing.*;
+
+
 public class App {
 	//ATTRIBUTES
 	// Current game
@@ -9,7 +13,30 @@ public class App {
 	private static GameHistory history;
 
 	public static void main(String[] args){
-	//this will serve as the main application
+		//this will serve as the main application
+		JFrame frame = new JFrame();
+		//set frame settings
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setFocusable(true);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setUndecorated(true);
+		frame.pack();
+		frame.setVisible(true);
 
+		Container cp = frame.getContentPane();
+
+		JPanel gameScreen = new JPanel();
+
+		gameScreen.setSize(new Dimension(frame.getWidth(), frame.getHeight()));
+
+		cp.add(gameScreen);
+
+		//get screen pixels
+		int screenWidth = gameScreen.getWidth();
+		int screenHeight = gameScreen.getHeight();
+
+		//debug
+		System.out.println(screenWidth);
+		System.out.println(screenHeight);
 	}
 }
